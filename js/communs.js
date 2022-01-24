5086ccdba9396f4840ac84598bbc4ea042fa0dba
Lenitra <lenitramc@gmail.com>
2022-01-24 15:53:51 +0100

    poaaah

---
+++ js/communs.js
@@ -0,0 +1,25 @@
+// barre de nav + logo + menu de navigation
+
+
+function header() {
+    document.write(`
+        <div class="logo">
+            <a href="index.html"><img src="imgs/logo.png" alt="logo" class="logo"/></a>
+        </div>
+
+        <img src="imgs/icons/menu.png" alt="" id="navigationbutton">
+
+        <nav>
+            <ul>
+                <li><a href="index.html">Accueil</a></li>
+                <li><a href="#">Dessins</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <ul id="resaux">
+                <li id="insta"><a href="https://instagram.com/mathis_enguehard_design"><img src="imgs/icons/insta.png" alt=""></a></li>
+            </ul>
+        </nav>
+
+        `);
+}
+
